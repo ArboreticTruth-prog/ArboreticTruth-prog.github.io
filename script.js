58409f5c498a0de96e079b559e2f8748c61f15ce
ArboreticTruth-prog <75991217+ArboreticTruth-prog@users.noreply.github.com>
2021-11-26 15:35:34 -0700

    Fiddling with Signup/Login

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 let hideObjects = () =>{
 	document.getElementById("user_area").style.display = "flex"
+	document.getElementById("login_area").style.display = "none"
+	document.getElementById("signup_area").style.display = "none"
 }
 
-let getServerInfo = () =>{
+let signUp = () =>{
+	const button = document.getElementById('signup_button');
+	let usern = document.getElementById("sign_up_username").innerText
+	let userpass = document.getElementById("sign_up_password2").innerText
+	let useremail = document.getElementById("sign_up_email").innerText
+	const getData = () => {
+    	axios.post('http://24.67.64.8:8081/user/signup',{
+            username:usern,
+            password:userpass,
+            email:useremail,
+            auth:'token here'
+    
+    })
+    .then(function (response) {
+        console.log(response);
+    }).catch(err=>{
+        console.log(err)
+    })
+} 
+
+button.addEventListener('click',()=>{
+    getData();
+})
+get_users()
+}
+
+let get_users = () =>{
 	let req = new XMLHttpRequest()
-	req.open("GET", "http://24.67.64.8:8081")
+	req.open("GET", "http://24.67.64.8:8081/user/all")
 	req.onload = function () {
-		let data = JSON.parse(req.responseText)
-		console.log(data)
+		console.log(req.responseText)
 	}
-	req.send()
+	req.send()	
 }
 
 let user_loggingIn = (event) =>{
-	
 	if (event.keyCode == 13){
 	let user_name = document.getElementById("user_name")
 	let pass_word = document.getElementById("pass")
@@ -21,10 +47,8 @@ let user_loggingIn = (event) =>{
 	usern = user_name.value
 		if (usern == "admin" && passw == "password"){
 			word_list()
-			user_name.style.visibility='hidden' 
-			pass_word.style.visibility='hidden' 
 			hideObjects()
-			getServerInfo()
+			get_users()
 			}
 		event.preventDefault()
 		}
